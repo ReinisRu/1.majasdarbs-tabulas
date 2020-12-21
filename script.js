c907134a8b6527f6d1dc299920a3e59f7602a141
ReinisRu <rr@itfly.lv>
2020-12-21 12:27:32 +0200

    touch test

--- script.js
+++ script.js
@@ -3,7 +3,12 @@
 
 document.getElementsByClassName("con-5").ontouchstart = animateBlowOut;
 
-function animateBlowOut(event) {
-    event.preventDefault();
-    this.toggleClass('touch_effect');
+// function animateBlowOut(event) {
+//     event.preventDefault();
+//     this.toggleClass('touch_effect');
+// }
+
+function animateBlowOut() {
+    var element = document.getElementsByClassName("con-5. .fill-cont");
+    element.classList.toggle("touch");
 }
