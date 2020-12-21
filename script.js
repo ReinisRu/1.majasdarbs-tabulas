103fad80fe5cbd381980abea698e792c4b19902a
ReinisRu <rr@itfly.lv>
2020-12-21 11:35:16 +0200

    test active

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// piemērs no W3
+
+// document.getElementsByClassName ("con-5").ontouchstart = animateBlowOut;
+
+// function animateBlowOut() {
+
+
+
+//   .hover {
+//     -webkit-user-select: none;
+//     -webkit-touch-callout: none;        
+//     }
+
+// }
