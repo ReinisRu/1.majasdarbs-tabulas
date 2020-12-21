41a15b3ce182b5795df798b08cf9c2631377fc2b
ReinisRu <rr@itfly.lv>
2020-12-21 12:34:02 +0200

    touch 2

--- script.js
+++ script.js
@@ -3,12 +3,13 @@
 
 document.getElementsByClassName("con-5").ontouchstart = animateBlowOut;
 
-// function animateBlowOut(event) {
-//     event.preventDefault();
-//     this.toggleClass('touch_effect');
-// }
-
-function animateBlowOut() {
+function animateBlowOut(event) {
+    event.preventDefault();
     var element = document.getElementsByClassName("con-5. .fill-cont");
     element.classList.toggle("touch");
 }
+
+// function animateBlowOut() {
+//     var element = document.getElementsByClassName("con-5. .fill-cont");
+//     element.classList.toggle("touch");
+// }
