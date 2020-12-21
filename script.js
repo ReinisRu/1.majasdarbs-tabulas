aabdfddd9aa5949c88e401f2a55b1a718dee92dc
ReinisRu <rr@itfly.lv>
2020-12-21 12:50:26 +0200

    touch removed

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 
-// piemērs no W3
+// variants 1 
 
-document.getElementsByClassName("con-5").ontouchstart = animateBlowOut;
+// document.getElementsByClassName("con-5").ontouchstart = animateBlowOut;
 
-function animateBlowOut(event) {
-    event.preventDefault();
-    var element = document.getElementsByClassName("con-5. .fill-cont");
-    element.classList.toggle("touch");
-}
+// function animateBlowOut(event) {
+//     event.preventDefault();
+//     var element = document.getElementsByClassName("con-5. .fill-cont");
+//     element.classList.toggle("touch");
+// }
+
+// variants 2 
 
 // function animateBlowOut() {
 //     var element = document.getElementsByClassName("con-5. .fill-cont");
